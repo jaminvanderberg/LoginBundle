4f9ccdbc7a526d39a4c80ba42f91b85507d7c932
jaminvanderberg <jaminvanderberg@yahoo.com>
2017-04-19 11:50:51 -0500

    Setting up templates

--- jaminvLoginBundle.php
+++ jaminvLoginBundle.php
@@ -5,5 +5,7 @@ namespace jaminv\LoginBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class jaminvLoginBundle extends Bundle {
-
+    public function getParent() {
+        return 'FOSUserBundle';
+    }
 }
